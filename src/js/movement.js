93a815545554710af9d3766439bba0470357cb70
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-06-22 16:52:52 -0500

    Add repeating movement section

--- src/js/movement.js
+++ src/js/movement.js
@@ -1,9 +1,9 @@
-on("change:repeating_movement:movement_mph", (e) => {
+on("change:repeating_movement:mph", (e) => {
   getAttrs(["combat_combined_attacks"], (a) => {
     const feetPerMelee = Math.round((e.newValue * 5280) / 60 / 4);
     const attrs = {
-      repeating_movement_movement_ft_melee: feetPerMelee,
-      repeating_movement_movement_ft_attack: Math.round(
+      repeating_movement_ft_melee: feetPerMelee,
+      repeating_movement_ft_attack: Math.round(
         feetPerMelee / a.combat_combined_attacks
       ),
     };
@@ -11,14 +11,14 @@ on("change:repeating_movement:movement_mph", (e) => {
   });
 });
 
-on("change:repeating_movement:movement_ft_melee", (e) => {
+on("change:repeating_movement:ft_melee", (e) => {
   getAttrs(["combat_combined_attacks"], (a) => {
     const feetPerMelee = e.newValue;
     //const mph = Math.round((feetPerMelee * 4 * 60) / 5280);
     const mph = ((feetPerMelee * 4 * 60) / 5280).toFixed(1);
     const attrs = {
-      repeating_movement_movement_mph: mph,
-      repeating_movement_movement_ft_attack: Math.round(
+      repeating_movement_mph: mph,
+      repeating_movement_ft_attack: Math.round(
         feetPerMelee / a.combat_combined_attacks
       ),
     };
