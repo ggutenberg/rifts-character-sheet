d01c785ed1488b13ed603426d47a520c1d8a34cd
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-06-29 21:28:33 -0500

    Add button to retrieve the selected bonus IDs and apply them to the current profile

--- src/js/profiles.js
+++ src/js/profiles.js
@@ -3,9 +3,16 @@ on("change:repeating_profiles:bonus_ids", (e) => {
   const bonusIds = e.newValue.split(",");
   const bonusNameKeys = bonusIds.map((id) => `repeating_bonuses_${id}_name`);
   getAttrs(bonusNameKeys, (a) => {
-    console.log(a);
     const names = Object.values(a).reduce((acc, cur) => `${acc} ${cur}`, "");
     setAttrs({ repeating_profiles_bonus_names: names });
   });
-  //   setAttrs({ repeating_profiles_bonus_names: "abc" });
+});
+
+on("clicked:repeating_profiles:copybonusids", (e) => {
+  console.log("clicked:copybonusids", e);
+  getAttrs(["bonus_ids_output"], (a) => {
+    const attrs = {};
+    attrs[`repeating_profiles_bonus_ids`] = a.bonus_ids_output;
+    setAttrs(attrs);
+  });
 });
