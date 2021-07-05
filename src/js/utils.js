db20c3c68a1485eb133678b2de5ae824f6e0f194
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-07-04 22:18:08 -0500

    In theory, all fields are aggregating to repeating profiles, including attributes.

--- src/js/utils.js
+++ src/js/utils.js
@@ -445,20 +445,3 @@ async function getSectionIDsOrderedAsync(sectionName) {
     ];
   return ids;
 }
-
-var getSectionIDsOrdered = function (sectionName, callback) {
-  "use strict";
-  getAttrs([`_reporder_${sectionName}`], function (v) {
-    getSectionIDs(sectionName, function (idArray) {
-      let reporderArray = v[`_reporder_${sectionName}`]
-          ? v[`_reporder_${sectionName}`].toLowerCase().split(",")
-          : [],
-        ids = [
-          ...new Set(
-            reporderArray.filter((x) => idArray.includes(x)).concat(idArray)
-          ),
-        ];
-      callback(ids);
-    });
-  });
-};
