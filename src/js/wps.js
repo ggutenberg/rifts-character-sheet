9812da91e95b07e01dd2e6c7b812f5bd9373769e
Garth Gutenberg <garth.gutenberg@gmail.com>
2021-07-05 22:46:13 -0500

    Async wps

--- src/js/wps.js
+++ src/js/wps.js
@@ -1,4 +1,4 @@
-function addWpToBonuses(section, rowId, wpName) {
+async function addWpToBonuses(section, rowId, wpName) {
   console.log("addWpToBonuses", section, rowId);
   if (!section || !rowId || !wpName) {
     return;
@@ -9,24 +9,23 @@ function addWpToBonuses(section, rowId, wpName) {
   );
   const wpCombatId = `repeating_${section}_${rowId}_bonus_id`;
   wpAttrs.push(wpCombatId);
-  getAttrs(wpAttrs, (a) => {
-    const attrs = {};
-    let combatRowId;
-    if (a[wpCombatId]) {
-      combatRowId = a[wpCombatId];
-    } else {
-      combatRowId = generateRowID();
-      attrs[wpCombatId] = combatRowId;
-    }
-    REPEATING_BONUS_KEYS.forEach((key) => {
-      attrs[`repeating_bonuses_${combatRowId}_${key}`] =
-        a[`repeating_${section}_${rowId}_${key}`] || 0;
-    });
-    setAttrs(attrs);
+  const a = await getAttrsAsync(wpAttrs);
+  const attrs = {};
+  let combatRowId;
+  if (a[wpCombatId]) {
+    combatRowId = a[wpCombatId];
+  } else {
+    combatRowId = generateRowID();
+    attrs[wpCombatId] = combatRowId;
+  }
+  REPEATING_BONUS_KEYS.forEach((key) => {
+    attrs[`repeating_bonuses_${combatRowId}_${key}`] =
+      a[`repeating_${section}_${rowId}_${key}`] || 0;
   });
+  await setAttrsAsync(attrs);
 }
 
-function setWpRow(section, name, keyPrefix, level) {
+async function setWpRow(section, name, keyPrefix, level) {
   console.log("setWpRow", name, keyPrefix, level);
   const attrs = {};
   const levelBonuses = WP[name.toLowerCase()].slice(0, level);
@@ -37,10 +36,10 @@ function setWpRow(section, name, keyPrefix, level) {
     attrs[key] = action in totalBonuses ? totalBonuses[action] : 0;
   });
   attrs[`${keyPrefix}_level`] = level;
-  setAttrs(attrs);
+  await setAttrsAsync(attrs);
 }
 
-function setWp({
+async function setWp({
   section,
   wpName,
   newCharacterLevel,
@@ -62,98 +61,84 @@ function setWp({
     ? `repeating_${section}_${rowId}`
     : `repeating_${section}`;
   if (newWpLevel) {
-    setWpRow(section, wpName, keyPrefix, newWpLevel);
-    if (callback) {
-      callback(section, rowId, wpName);
-    }
+    await setWpRow(section, wpName, keyPrefix, newWpLevel);
+    // if (callback) {
+    //   callback(section, rowId, wpName);
+    // }
   } else if (!rowId) {
-    setWpRow(section, wpName, keyPrefix, newCharacterLevel);
-    if (callback) {
-      callback(section, rowId, wpName);
-    }
+    await setWpRow(section, wpName, keyPrefix, newCharacterLevel);
+    // if (callback) {
+    //   callback(section, rowId, wpName);
+    // }
   } else {
-    getAttrs([`${keyPrefix}_level`], (a) => {
-      const oldWpLevel = a[`${keyPrefix}_level`];
-      if (oldCharacterLevel) {
-        const delta = oldCharacterLevel - oldWpLevel;
-        if (delta != 0) {
-          newWpLevel = newCharacterLevel - delta;
-          setWpRow(section, wpName, keyPrefix, newWpLevel);
-        } else {
-          setWpRow(section, wpName, keyPrefix, newCharacterLevel);
-        }
+    const a = await getAttrsAsync([`${keyPrefix}_level`]);
+    const oldWpLevel = a[`${keyPrefix}_level`];
+    if (oldCharacterLevel) {
+      const delta = oldCharacterLevel - oldWpLevel;
+      if (delta != 0) {
+        newWpLevel = newCharacterLevel - delta;
+        await setWpRow(section, wpName, keyPrefix, newWpLevel);
       } else {
-        setWpRow(section, wpName, keyPrefix, newCharacterLevel);
+        await setWpRow(section, wpName, keyPrefix, newCharacterLevel);
       }
-      if (callback) {
-        callback(section, rowId, wpName);
-      }
-    });
+    } else {
+      await setWpRow(section, wpName, keyPrefix, newCharacterLevel);
+    }
+    // if (callback) {
+    //   callback(section, rowId, wpName);
+    // }
   }
+  // return { section };
 }
 
-function updateWeaponProficiencies(
+async function updateWeaponProficiencies(
   section,
   newCharacterLevel,
   oldCharacterLevel
 ) {
-  getSectionIDs(section, (ids) => {
-    const attrNames = ids.map((id) => `repeating_${section}_${id}_name`);
-    getAttrs(attrNames, (a) => {
-      ids.forEach((id) => {
-        setWp({
-          section,
-          wpName: a[`repeating_${section}_${id}_name`].toLowerCase(),
-          newCharacterLevel,
-          oldCharacterLevel,
-          rowId: id,
-        });
-      });
+  const ids = await getSectionIDsAsync(section);
+  const attrNames = ids.map((id) => `repeating_${section}_${id}_name`);
+  const a = await getAttrsAsync(attrNames);
+  for (rowId of ids) {
+    await setWp({
+      section,
+      wpName: a[`repeating_${section}_${rowId}_name`].toLowerCase(),
+      newCharacterLevel,
+      oldCharacterLevel,
+      rowId,
     });
-  });
+  }
 }
 
-function updateWeaponProficiency(section, source, newWpLevel) {
-  getSectionIDs(section, (ids) => {
-    const rowId = ids.find(
-      (id) => `repeating_${section}_${id}_level`.toLowerCase() == source
-    );
-    getAttrs([`repeating_${section}_${rowId}_name`], (a) => {
-      setWp({
-        section,
-        wpName: a[`repeating_${section}_${rowId}_name`],
-        newWpLevel,
-        rowId: rowId,
-        callback: addWpToBonuses,
-      });
-    });
-  });
+async function updateWeaponProficiency(section, source, newWpLevel) {
+  const ids = await getSectionIDsAsync(section);
+  const rowId = ids.find(
+    (id) => `repeating_${section}_${id}_level`.toLowerCase() == source
+  );
+  const a = await getAttrsAsync([`repeating_${section}_${rowId}_name`]);
+  const wpName = a[`repeating_${section}_${rowId}_name`];
+  await setWp({ section, wpName, newWpLevel, rowId });
+  await addWpToBonuses(section, rowId, wpName);
 }
 
-on("change:repeating_wp:level change:repeating_wpmodern:level", (e) => {
+on("change:repeating_wp:level change:repeating_wpmodern:level", async (e) => {
   const section = e.sourceAttribute.split("_")[1];
-  updateWeaponProficiency(section, e.sourceAttribute, e.newValue);
+  await updateWeaponProficiency(section, e.sourceAttribute, e.newValue);
 });
 
-on("change:repeating_wp:name change:repeating_wpmodern:name", (e) => {
+on("change:repeating_wp:name change:repeating_wpmodern:name", async (e) => {
   console.log("change:repeating_wp:name change:repeating_wpmodern:name", e);
   const [r, section, rowId, attr] = e.sourceAttribute.split("_");
   const wpName = e.newValue.toLowerCase();
-  const wpLevelKey = `repeating_${section}_level`;
-  setAttrs({ [`repeating_${section}_rowid`]: `repeating_${section}_${rowId}` });
-  getAttrs(["character_level", wpLevelKey], (a) => {
-    if (Boolean(a[wpLevelKey]) && +a[wpLevelKey] != 0) {
-      setWp({
-        section,
-        wpName,
-        rowId,
-        newWpLevel: a[wpLevelKey],
-        callback: addWpToBonuses,
-      });
-    } else {
-      const attrs = {};
-      attrs[`repeating_${section}_level`] = a.character_level;
-      setAttrs(attrs);
-    }
-  });
+  const wpLevelKey = `${r}_${section}_${rowId}_level`;
+  setAttrs({ [`${r}_${section}_${rowId}_rowid`]: `${r}_${section}_${rowId}` });
+  const a = await getAttrsAsync(["character_level", wpLevelKey]);
+  if (Boolean(a[wpLevelKey]) && +a[wpLevelKey] != 0) {
+    await setWp({ section, wpName, rowId, newWpLevel: a[wpLevelKey] });
+    await addWpToBonuses(section, rowId, wpName);
+  } else {
+    const attrs = {};
+    attrs[`${r}_${section}_${rowId}_level`] = a.character_level;
+    await setAttrsAsync(attrs);
+  }
 });
